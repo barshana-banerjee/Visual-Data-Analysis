fb432917cc06a0f56daf4f870ff5aceb12dd9811
barshana-banerjee <banerjee.barshana@gmail.com>
2019-05-08 15:53:34 +0200

    Multidimensional Data Filtering and Visualization
    A numerical rating of sensory wine quality is given in the column“quality”. Display the distribution
    of these scores with a histogram. What is the range of this score in the data? 
    c) Derive a coarser classification of quality into “low”, “medium”, and “high”, by grouping together
    the two lowest, the intermediate, and the two highest quality scores that occur in the dataset,
    respectively. Replace the original “quality” column with a new column “quality bin” that contains
    these labels. 
    d) We would like to investigate differences between high and low quality wines. Therefore, create a
    filtered data frame in which the medium-quality wines are omitted. 
    e) Visualize all numerical attributes in a scatterplot matrix. Color the two quality levels differently.
    Hint: Using seaborn, you can create this plot with a single (and simple) line of code.
    f) Based on the visualization, name five attributes that appear to best distinguish between high and
    low quality. 
    g) Now, use an automated feature selection technique to identify five attributes that distinguish
    between high and low quality. More specifically, please use the F score from a one-way analysis
    of variance (ANOVA) to rank the attributes, as implemented in scikit learn’s f_classif. What
    are the five best attributes according to this measure? Are they the same as those you identified
    visually? Create a filtered data frame that only contains the top five attributes, plus the “quality
    bin”.

---
+++ Multidimensional Data Filtering and Visualization.py
@@ -0,0 +1,91 @@
+'''
+Submitted by:
+Barshana Banerjee
+Hemanth Kumar Reddy Mayaluru
+'''
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_classif
+from sklearn import preprocessing
+
+# 1(a) - Reading the data and printing first few rows
+data = pd.read_csv('winequality-red-actual.csv')
+data.head()
+
+
+# 1(b) - Histogram for the distribution of column "quality" and it's range
+sns.distplot(data['quality'],  color='green');
+range_q = data["quality"].max() - data["quality"].min()
+print('Range: ', range_q)
+
+
+#1(c) - classification of quality into "low", "medium", and "high"
+#bins = (0,data['quality'].nsmallest(2).min(),data['quality'].nlargest(2).min()-1,data['quality'].nlargest(2).min())
+bins = (0,4,7,8)
+data.insert(12,'quality bin',1)
+group_names = ['low', 'medium', 'high']
+data['quality bin'] = pd.cut(data['quality'], bins = bins, labels = group_names)
+data.drop('quality',1)
+del data['quality']
+data.head()
+data.to_csv('winequality-red-actual.csv',index=False)
+
+
+#1(d) - filtered data frame in which the medium-quality wines are omitted
+data_filtered = data.loc[data['quality bin'] != 'medium']
+#data_filtered.head(20)
+data_filtered2 = data_filtered
+data_filtered3 = data_filtered
+data_filtered.head(20)
+
+
+#1(e) - scatterplot matrix
+ax = sns.pairplot(hue='quality bin', markers=False, data=data_filtered)
+
+
+''' 1(f) - Based on the visualization, below are the five attributes that appear to best distinguish between high and
+           low quality:
+                - fixed acidity
+                - volatile acidity
+                - pH
+                - sulphates
+                - alcohol           
+'''
+
+# 1(g) -  automated feature selection technique to identify five attributes that distinguish between high and low quality
+
+#Create features and target
+X = data_filtered2
+y = 'quality bin'
+
+# Create an SelectKBest object to select features with two best ANOVA F-Values
+fvalue_selector = SelectKBest(f_classif, k=5)
+data_filtered2['quality bin'] = np.where(data_filtered2['quality bin'] == 'high', 1, 0)
+
+# Apply the SelectKBest object to the features and target
+X_kbest = fvalue_selector.fit_transform(data_filtered2,data_filtered2['quality bin'])
+cols=fvalue_selector.get_support(indices=True)
+colname=[]
+print("Features:")
+for i in cols:
+    colname.append(data_filtered2.columns[i-1])  
+    print(" - ", data_filtered2.columns[i-1])
+
+# Show results
+print('Original number of features:', X.shape[1])
+print('Reduced number of features:', X_kbest.shape[1])
+
+
+#1(h) - Create a matrix similar to the one in Fig. 1. Legend (1  - for high, 0 - for low)
+g = sns.PairGrid(data_filtered, hue='quality bin',y_vars=['volatile acidity','citric acid','pH','sulphates','alcohol'],x_vars =['volatile acidity','citric acid','pH','sulphates','alcohol'])
+g.map_upper(plt.scatter)
+g.map_lower(sns.regplot,scatter=False)
+g.map_diag(sns.kdeplot, lw=3, legend=True);
+g.add_legend()
+
+
+
