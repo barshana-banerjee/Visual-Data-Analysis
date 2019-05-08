65280e1a1c2e0d6e96f2f809499262e2c2eeec39
barshana-banerjee <banerjee.barshana@gmail.com>
2019-05-08 15:49:59 +0200

    Plotting Categorical Data
    Use pandas.melt to transform the data from “wide” to “long” format, using class (indicating
    ckd for chronic kidney disease or notckd for its absence) as the identifier variable. 
    c) For each numerical attribute, such as age or blood pressure, create two boxplots side-by-side. One
    should show the attribute’s distribution among patients suffering from chronic kidney disease, the
    other one from patients who do not suffer from the disease. Hint: Due to the different numerical
    ranges, you will have to disable sharing of y axes between plots of different attributes. 
    d) Based on viewing the plots, name an attribute that appears to be highly indicative of chronic
    kidney disease, and one that seems to be mostly unrelated to it.

---
+++ vda4.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[47]:
+
+
+# Name- 1. Barshana Banerjee, 2. Hemanth Kumar Reddy Mayaluru
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+excel_file = 'chronic_kidney_disease_numerical.xls'
+data = pd.read_excel(excel_file)
+
+d = pd.melt(data, id_vars='class',value_vars=None,var_name=None, value_name='value', col_level=None)
+print(d)
+
+b = sns.boxplot(data = df_melt,
+                hue = 'class', # different colors for different 'cls'
+                x = 'columns',
+                y = 'value',
+                order = ['age', # custom order of boxplots
+                         'blood',
+                         'albumin',
+                         'sugar',
+                         'blood pressure',
+                        'blood glucose random',
+                        'blood urea',
+                        'white blood cell count',
+                        'red blood cell count'
+                        ])
+
+plt.title('Boxplot grouped by class') # You can change the title here
+b.set_xticklabels(b.get_xticklabels(), rotation=40, ha="right")
+plt.tight_layout()
+plt.show()
+
+
+# In[ ]:
+
+
+
+
